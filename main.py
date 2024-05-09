168757718488823586b387f45501293d9427f41b
Maks891 <124050452+Maks891@users.noreply.github.com>
2024-05-09 18:35:20 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,6 @@
 import os
 from aiogram import Bot, Dispatcher, types
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
-from config import *
 from commands.main import *
 from commands.help import *
 from commands.balance import *
