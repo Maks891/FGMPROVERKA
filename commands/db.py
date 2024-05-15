de960b6b182ff88eeee8ff4dee1d1949bc6b338b
Maks891 <124050452+Maks891@users.noreply.github.com>
2024-05-15 05:18:31 +0300

    Update db.py

--- commands/db.py
+++ commands/db.py
@@ -167,10 +167,6 @@ async def getlimitdb(message):
     i = cursor.execute('SELECT per FROM users WHERE user_id = ?', (user_id,)).fetchone()[0]
     return int(i)
 
-async def getlimitbtcdb(message):
-    user_id = message.from_user.id
-    i = cursor.execute('SELECT perbtc FROM users WHERE user_id = ?', (user_id,)).fetchone()[0]
-    return int(i)
   
 async def getads(message=None):
     ads = cursor.execute("SELECT ads FROM sett").fetchone()[0]
