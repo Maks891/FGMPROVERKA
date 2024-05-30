2b300dc17658af934ba3f1ccde70ab0ec4ffaf80
Maks891 <124050452+Maks891@users.noreply.github.com>
2024-05-30 10:02:10 +0300

    Update main.py

--- commands/property/main.py
+++ commands/property/main.py
@@ -240,6 +240,19 @@ async def my_house(message: types.Message):
     hdata = house.get(data[5])
     await bot.send_photo(chat_id=message.chat.id, photo=hdata[1], caption=f'{name}, ваш дом "{hdata[0]}"')
 
+@antispam
+async def my_pet(message: types.Message):
+    name = await url_name(message.from_user.id)
+    rwin, rloser = await win_luser()
+    data = await db.get_property(message.from_user.id)
+    if data[5] == 0:
+        await message.answer(f'{name}, к сожалению у вас нет своего питомца {rloser}')
+        return
+
+    hdata = pet.get(data[5])
+    await bot.send_photo(chat_id=message.chat.id, photo=hdata[1], caption=f'{name}, ваш питомец "{hdata[0]}"')
+
+
 
 @antispam
 async def my_yahta(message: types.Message):
