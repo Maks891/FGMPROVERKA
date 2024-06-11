efd558299a4f973af980f24f9471f65016c5ebb6
Maks891 <124050452+Maks891@users.noreply.github.com>
2024-06-11 20:16:26 +0300

    Update loger.py

--- commands/admin/loger.py
+++ commands/admin/loger.py
@@ -1,15 +1,15 @@
 from bot import bot
-import config as cfg
+import config_ex as cfg
 
 # 0 - отключить логирование
 # 1 - отправлять логи только в логчат
 # 2 - записывать логи только в файл logs.txt
 # 3 - записывать логи в файл и отправлять в логчат
 
-promo = 3  # активация промо
-money_transfers = 3  # перевод денег
-issuance_money = 3  # выдача денег
-issuance_bcoins = 3  # выдача бкоинов
+promo = 1  # активация промо
+money_transfers = 1  # перевод денег
+issuance_money = 1  # выдача денег
+issuance_bcoins = 1  # выдача бкоинов
 
 
 async def new_log(txt, log_type):
@@ -28,7 +28,7 @@ async def new_log(txt, log_type):
     log_message = f'новый лог: {txt}'
 
     if logging_level == 1 or logging_level == 3:
-        try: await bot.send_message(chat_id=cfg.chat_log, text=log_message)
+        try: await bot.send_message(chat_id=-1002247244049, text=log_message)
         except: print('указан не верный айди чата для логов')
 
     if logging_level == 2 or logging_level == 3:
