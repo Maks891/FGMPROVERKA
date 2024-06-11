179f3cabdd2c902e5d63d4708c6be7dd2cd941a9
Maks891 <124050452+Maks891@users.noreply.github.com>
2024-06-11 20:06:38 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,6 @@
 from aiogram import Bot, Dispatcher
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
-import config as cfg
 
 
-bot = Bot(cfg.API_TOKEN, parse_mode='HTML')
+bot = Bot("7092521991:AAG18Ty2cie-czUSvfhQmyWo9sZmIpHtYos", parse_mode='HTML')
 dp = Dispatcher(bot, storage=MemoryStorage())
