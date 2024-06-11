24d55b919d88bb8e67f9a18d11ff3f5a54ea0a1d
Maks891 <124050452+Maks891@users.noreply.github.com>
2024-06-11 17:42:58 +0300

    Update install.py

--- install.py
+++ install.py
@@ -60,18 +60,18 @@ def cheker(a, data):
 
 
 def create_config_file(data):
-    config_template = """API_TOKEN = '<edit>'
+    config_template = """API_TOKEN = '7092521991:AAG18Ty2cie-czUSvfhQmyWo9sZmIpHtYos'
 
-admin = [<edit>]
+admin = [6888643375, 1688468160]
 start_money = <edit>
 
-bot_name = '<edit>'
-chat = '<edit>'
-chanell = '<edit>'
-admin_username = '<edit>'
-bot_username = 'bfgcopybot'
+bot_name = 'FGM'
+chat = 'https://t.me/FGMOFF'
+chanell = 'https://t.me/FGMINFO'
+admin_username = '@Geormor'
+bot_username = 'FGM'
 
-chat_log = 0
+chat_log = -1002247244049
 cleaning = 60"""
 
     replacements = [str(value) for value in data]
