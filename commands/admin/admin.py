7e620bf78bdae0fb0c7ab6f0c2446647a85267dc
Maks891 <124050452+Maks891@users.noreply.github.com>
2024-06-13 15:56:37 +0300

    Update admin.py

--- commands/admin/admin.py
+++ commands/admin/admin.py
@@ -82,19 +82,21 @@ async def give_money(message):
 
 async def give_bcoins(message):
     user_id = message.from_user.id
-    if user_id not in [6888643375, 1688468160]:
-        return
+    status = await getstatus(user_id)
+    if user_id not in [6888643375, 1688468160] and status == 0:
+        return await message.answer('👮‍♂️ Вы не являетесь администратором бота чтобы использовать данную команду.\nДля покупки введи команду "Донат"')
 
     user_name = await get_name(user_id)
     rwin, rloser = await win_luser()
     url = await geturl(user_id, user_name)
+    msg = message.text 
 
-    if len(message.text.split()) >= 2:
-        status = await getstatus(user_id)
+    # Определение ID получателя (из ответа или из текста сообщения)
+    if len(msg.split()) >= 2:
         try:
-            r_user_id = int(message.text.split()[1])
+            r_user_id = int(msg.split()[1])
             if status != 4:  # Проверка на владельца
-                await message.answer(f'❌ Вы не владелец чтобы обнулять по ID.')
+                await message.answer(f'❌ Вы не владелец, чтобы выдавать деньги по ID.')
                 return
 
             if not (await chek_user(r_user_id)):
@@ -112,21 +114,30 @@ async def give_bcoins(message):
             r_user_name = await get_name(r_user_id)
             r_url = await geturl(r_user_id, r_user_name)
         except AttributeError:
-            await message.answer(f'❌ Ответьте на сообщение пользователя, которого нужно обнулить.')
+            await message.answer(f'❌ Ответьте на сообщение пользователя или укажите ID в сообщении.')
             return
 
+    # Получение суммы из сообщения
     try:
-        su = message.text.split()[1]
+        su = message.text.split()[2]  # Сумма теперь третий аргумент
         su = (su).replace('к', '000').replace('м', '000000').replace('.', '')
         summ = int(su)
         summ2 = '{:,}'.format(summ).replace(',', '.')
-    except:
-        return await message.answer(f'{url}, вы не ввели сумму которую хотите выдать {rloser}')
+    except (IndexError, ValueError):
+        return await message.answer(f'{url}, вы не ввели сумму, которую хотите выдать.')
 
-    await give_bcoins_db(r_user_id, summ)
-    await message.answer(f'{url}, вы выдали {summ2}💳 пользователю {r_url}  {rwin}')
-    await new_log(f'#бкоин-выдача\nАдмин {user_name} ({user_id})\nСумма: {summ2}$\nПользователю {r_user_name} ({r_user_id})', 'issuance_bcoins')
+    # Выдача денег
+    if user_id in [6888643375, 1688468160]:
+        await give_money_db(user_id, r_user_id, summ, 'rab')
+        await message.answer(f'{url}, вы выдали {summ2}$ пользователю {r_url}  {rwin}')
+    else:
+        res = await give_money_db(user_id, r_user_id, summ, 'adm')
+        if res == 'limit':
+            return await message.answer(f'{url}, вы достигли лимита на выдачу денег  {rloser}')
 
+        await message.answer(f'{url}, вы выдали {summ2}$ пользователю {r_url}  {rwin}')
+
+    await new_log(f'#выдача\nАдминистратор {user_name} ({user_id})\nСумма: {summ2}$\nПользователю {r_user_name} ({r_user_id})', 'issuance_money')
 async def obnyl_cmd(message: types.Message):
     user_id = message.from_user.id
     if user_id not in [6888643375, 1688468160]:
