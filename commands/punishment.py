99c1a2347bd6a72e45483cfa00fecc06ebad9e45
Maks891 <124050452+Maks891@users.noreply.github.com>
2024-06-13 13:06:26 +0300

    Update punishment.py

--- commands/punishment.py
+++ commands/punishment.py
@@ -1,3 +1,4 @@
+from datetime import datetime, timedelta
 from aiogram import types, Dispatcher
 from aiogram.dispatcher import FSMContext
 from aiogram.dispatcher.filters.state import StatesGroup, State
